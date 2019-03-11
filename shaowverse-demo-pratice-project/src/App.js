e137b9f2bebe0ff473c3d1b9124c7fe8c9267776
guohongjian <2844735407@qq.com>
2019-03-11 13:12:26 +0800

    毛玻璃特效

--- shaowverse-demo-pratice-project/src/App.js
+++ shaowverse-demo-pratice-project/src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import test from './assets/svg/test.svg'
 import './App.css';
 
 class App extends Component {
+  componentDidMount () {
+      console.log('生命周期componentDidMount')
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<p>*/}
+            {/*Edit <code>src/App.js</code> and save to reload.*/}
+          {/*</p>*/}
+          {/*<a*/}
+            {/*className="App-link"*/}
+            {/*href="https://reactjs.org"*/}
+            {/*target="_blank"*/}
+            {/*rel="noopener noreferrer"*/}
+          {/*>*/}
+            {/*Learn React*/}
+          {/*</a>*/}
+            <div>
+                <img src={test} type="image/svg + xml" alt="" />
+            </div>
         </header>
+          <div className="App-content">
+            <article className="wrap">
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+                <p>练习毛玻璃效果</p>
+            </article>
+            <div className="">
+
+            </div>
+          </div>
+        <footer className="App-footer">
+          <div>personal project</div>
+        </footer>
       </div>
     );
   }
